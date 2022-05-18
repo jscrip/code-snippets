a5a5021041ccbb76a811de0fec2d95a6824bad0b
Jeremy B <jscrip@users.noreply.github.com>
2022-05-18 15:33:42 -0400

    Create form-data-on-input.js
    init

---
+++ javascript/client/userscripts-and-injections/quality-of-life-hacks/form-data-on-input.js
@@ -0,0 +1,17 @@
+/*Select the form*/
+var form = document.querySelector('form');
+/* listen for live input events on the form.
+   The form is logged every time a change is made.
+   
+*/
+form.addEventListener('input', (e) => {
+      e.preventDefault();
+      /* get all form data as of input event*/
+      var fData = new FormData(form);
+      /* Parse Data */
+      var data = [...fData.entries()].reduce((o,d) =>{
+        o[d[0]] = d[1];
+        return o;
+      },{});
+      console.log({data})
+});
